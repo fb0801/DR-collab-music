102b1e33604ea86b65ecd9b8b16800b5fc14c10d
fb0801 <fbhatti0801@gmail.com>
2023-06-09 19:55:16 +0100

    django POST

--- myworld/music_controller/api/views.py
+++ myworld/music_controller/api/views.py
@@ -13,6 +13,26 @@ class RoomView(generics.CreateAPIView):
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
-    
+
     def post(self, request, format=None):
-        pass
+        if not self.request.session.exists(self.request.session.session_key):
+            #not active session
+            self.request.session.create()
+
+            serializer = self.serializer_class(data= request.data)
+            if serializer.is_valid():
+                guest_can_pause = serializer.data.get('guest_can_pause')
+                votes_to_skip = serializer.data.get('votes_to_skip')
+                host = self.request.session.session_key
+                queryset = Room.objects.filter(host=host)
+                if queryset.exists():
+                    room = queryset[0]
+                    room.guest_can_pause = guest_can_pause
+                    room.votes_to_skip = votes_to_skip
+                    room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+
+                else:
+                    room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip= votes_to_skip)
+                    room.save()
+
+                return Response(RoomSerializer(room).data, status=status)
