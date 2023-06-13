f4cbc654bcabcc3d31b000e09cfe60e88ab618d9
fb0801 <fbhatti0801@gmail.com>
2023-06-13 18:57:31 +0100

    create room join 2

--- myworld/music_controller/api/views.py
+++ myworld/music_controller/api/views.py
@@ -28,33 +28,51 @@ class GetRoom(APIView):
         return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
     
 class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
     def post(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
             #not active session
             self.request.session.create()
+
+            code =  request.data.get(self.lookup_url_kwarg)
+            if code != None:
+                room_result = Room.objects.filter(code = code)
+                if len(room_result) > 0:
+                    room = room_result[0]
+                    self.request.session['room_code'] = code
+                    return Response({'message':'Room Joined'}, status=status.HTTP_200_OK)
+                                    
+                return Response({'BAD Request':'invalid room code'}, status=status.HTTP_400_BAD_REQUEST)
+
+            return Response({'bad request': 'invalid post no key found'}, status=status.HTTP_400_BAD_REQUEST)    
+                
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
 
     def post(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
-            #not active session
+            #not active
             self.request.session.create()
 
-            serializer = self.serializer_class(data= request.data)
-            if serializer.is_valid():
-                guest_can_pause = serializer.data.get('guest_can_pause')
-                votes_to_skip = serializer.data.get('votes_to_skip')
-                host = self.request.session.session_key
-                queryset = Room.objects.filter(host=host)
-                if queryset.exists():
-                    room = queryset[0]
-                    room.guest_can_pause = guest_can_pause
-                    room.votes_to_skip = votes_to_skip
-                    room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            votes_to_skip=votes_to_skip)
+                room.save()
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
-                else:
-                    room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip= votes_to_skip)
-                    room.save()
-
-                return Response(RoomSerializer(room).data, status=status)
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
