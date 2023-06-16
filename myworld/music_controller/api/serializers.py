03380d29571bba24a23d5abda2f9ac4dcf8679b7
fb0801 <fbhatti0801@gmail.com>
2023-06-16 15:03:17 +0100

    django models update 1

--- myworld/music_controller/api/serializers.py
+++ myworld/music_controller/api/serializers.py
@@ -5,10 +5,19 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        feilds = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+        fields = ('id', 'code', 'host', 'guest_can_pause',
+                  'votes_to_skip', 'created_at')
 
 
 class CreateRoomSerializer(serializers.ModelSerializer):
-     class Meta:
+    class Meta:
         model = Room
-        feilds = ('guest_can_pause', 'votes_to_skip')
+        fields = ('guest_can_pause', 'votes_to_skip')
+
+
+class UpdateRoomSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(validators=[])
+
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip', 'code')
