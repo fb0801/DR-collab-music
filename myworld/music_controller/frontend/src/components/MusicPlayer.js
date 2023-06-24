b394757de432fa181bb813955db2f3bc18cdf466
fb0801 <fbhatti0801@gmail.com>
2023-06-24 18:20:33 +0100

    skip song 3

--- myworld/music_controller/frontend/src/components/MusicPlayer.js
+++ myworld/music_controller/frontend/src/components/MusicPlayer.js
@@ -64,7 +64,8 @@ export default class MusicPlayer extends Component {
                 {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
               </IconButton>
               <IconButton onClick={() => this.skipsong()}>
-                <SkipNextIcon />
+                {this.props.votes} / {' '} {this.props.votes_required}
+                <SkipNextIcon /> 
               </IconButton>
             </div>
           </Grid>
